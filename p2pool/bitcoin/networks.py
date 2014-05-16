d043994240d86777d86a10f2afdd04255f18de65
paniczklos <panicz.klos@gmail.com>
2014-05-16 17:58:34 +0200

    rightcoin
    rightcoin

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -45,7 +45,28 @@ nets = dict(
         DUMB_SCRYPT_DIFF=1,
         DUST_THRESHOLD=0.001e8,
     ),
-
+ rightcoin=math.Object(
+        P2P_PREFIX='xx'.decode('hex'),
+        P2P_PORT=xx,
+        ADDRESS_VERSION=xx,
+        RPC_PORT=xx,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'rightcoinaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),                           
+        SUBSIDY_FUNC=lambda bitcoind, target: get_subsidy(bitcoind, target),
+        BLOCKHASH_FUNC=lambda data: pack.IntType(256).unpack(__import__('xcoin_hash').getPoWHash(data)),
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('xcoin_hash').getPoWHash(data)),
+        BLOCK_PERIOD=30, # s
+        SYMBOL='RTC',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'rightcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/rightcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.rightcoin'), 'rightcoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='',
+        ADDRESS_EXPLORER_URL_PREFIX='',
+        TX_EXPLORER_URL_PREFIX='',
+        SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**20 - 1), 
+        DUMB_SCRYPT_DIFF=1,
+        DUST_THRESHOLD=0.001e8,
+    ),
 )
 for net_name, net in nets.iteritems():
     net.NAME = net_name

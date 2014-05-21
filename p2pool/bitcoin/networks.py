c7cdfe1e033cbc0d3457d7e79b80a4930ac11c58
paniczklos <panicz.klos@gmail.com>
2014-05-21 10:54:22 +0200

    Update networks.py

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -67,6 +67,28 @@ nets = dict(
         DUMB_SCRYPT_DIFF=1,
         DUST_THRESHOLD=0.001e8,
     ),
+antarcticcoin=math.Object(
+        P2P_PREFIX='fec3b9de'.decode('hex'),
+        P2P_PORT=9164,
+        ADDRESS_VERSION=23,
+        RPC_PORT=9163,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'antarcticCoinaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),                           
+        SUBSIDY_FUNC=lambda bitcoind, target: get_subsidy(bitcoind, target),
+        BLOCKHASH_FUNC=lambda data: pack.IntType(256).unpack(__import__('xcoin_hash').getPoWHash(data)),
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('xcoin_hash').getPoWHash(data)),
+        BLOCK_PERIOD=60, # s
+        SYMBOL='ACC',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'antarcticCoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/antarcticCoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.antarcticCoin'), 'antarcticCoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='',
+        ADDRESS_EXPLORER_URL_PREFIX='',
+        TX_EXPLORER_URL_PREFIX='',
+        SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**20 - 1), 
+        DUMB_SCRYPT_DIFF=1,
+        DUST_THRESHOLD=0.001e8,
+    ),    
 )
 for net_name, net in nets.iteritems():
     net.NAME = net_name
